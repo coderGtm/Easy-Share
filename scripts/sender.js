ee0199743862a2d376eaf414411f002cf3b8cbd4
coderGtm <gautammehta0000@gmail.com>
2022-07-16 13:34:48 +0530

    worked on sender script

--- scripts/sender.js
+++ scripts/sender.js
@@ -1,8 +1,27 @@
-window.onload = function(loadEvt) {
+window.onload = function(_loadEvt) {
     peerCode = getRandomNum();
     const peer = new Peer("coderGtm-Easy-Share-"+peerCode);
     peer.on('open', function(id) {
-        
+        alert(id)
+        document.getElementById("peerCode").innerText = peerCode;
+        document.getElementById("connectionWaiting").style.display = "flex";
+        connected = false;
+        peer.on("connection", function(conn) {
+            if (connected) {
+                //Connection aborted
+                conn.close();
+                return;
+            }
+            connection = conn;
+            document.getElementById("connectionWaiting").style.display = "none";
+            document.getElementById("connectionConnected").style.display = "block";
+            connection.on("open", function() {
+                alert("open");
+            })
+        });
+        peer.on("error", function(err) {
+            alert("An error occured. Please try again.");
+        });
     });
 }
 function getRandomNum() {
