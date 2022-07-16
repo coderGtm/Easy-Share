741a4e0a0d6dfc8d9f19fc134f017b3b9150fb5b
coderGtm <gautammehta0000@gmail.com>
2022-07-16 14:10:28 +0530

    UI and logic upto connection established for sender

--- scripts/sender.js
+++ scripts/sender.js
@@ -2,7 +2,6 @@ window.onload = function(_loadEvt) {
     peerCode = getRandomNum();
     const peer = new Peer("coderGtm-Easy-Share-"+peerCode);
     peer.on('open', function(id) {
-        alert(id)
         document.getElementById("peerCode").innerText = peerCode;
         document.getElementById("connectionWaiting").style.display = "flex";
         connected = false;
@@ -15,13 +14,16 @@ window.onload = function(_loadEvt) {
             connection = conn;
             document.getElementById("connectionWaiting").style.display = "none";
             document.getElementById("connectionConnected").style.display = "block";
+            setTimeout(function() {
+                document.getElementById("connectionInterface").classList.add("fade-out-top");
+            },2000);
             connection.on("open", function() {
-                alert("open");
+                
             })
         });
     });
     peer.on("error", function(err) {
-        console.log(err);
+        console.error(err);
         alert("An error occured. Please try again.");
     });
 }
